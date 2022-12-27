7ea9d40618c6d8f6a5046a1f8a1b1f645bdc60bc
hrshlmeht <harshalmehta1998@gmail.com>
2022-12-27 01:09:39 -0500

    username and unique id generated

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,21 @@
-import React from 'react'
+import React, {useState} from 'react'
+import {v4 as uuidv4} from 'uuid'
 
 const Home = () => {
+
+  const [roomId, setRoomId] = useState('')
+
+  const [userName, setuserName] = useState('')
+
+  const createNewRoom = (e) => {
+    e.preventDefault();
+    const id = uuidv4();
+    setRoomId(id)
+    console.log(id);
+
+
+  }
+
   return (
     <div className='homePageWrapper'>
       <div className='formPageWrapper'>
@@ -10,17 +25,26 @@ const Home = () => {
           <input
             type = 'text' 
             className='inputBox'
-            placeholder= "Room ID" 
+            placeholder= "Room ID"
+            onChange={(e)=> setRoomId(e.target.value)}
+            value = {roomId}  
           />
           <input
             type = 'text' 
             className='inputBox'
             placeholder= "Username" 
+            onChange={(e)=> setuserName(e.target.value)}
+            value = {userName}
           />
           <button className='btn joinBtn'>Join</button>
           <span className='CreateInfo'>
             If you dont have an invite then create &nbsp;
-            <a href=' 'className='createNewBtn'>new room</a>
+            <a 
+              onClick={createNewRoom} 
+              href=' '
+              className='createNewBtn'>
+              new room
+            </a>
           </span>  
         </div>
       </div>
