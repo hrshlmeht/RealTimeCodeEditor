2a7b13d3311afc1deb30d85411b402e62d565c4b
hrshlmeht <harshalmehta1998@gmail.com>
2022-12-29 02:50:39 -0500

    strcuture of editorpage done

---
+++ src/components/Editor.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Editor = () => {
+  return (
+    <div>Editor goes here </div>
+  )
+}
+
+export default Editor
--- src/pages/CodeEditor.js
+++ src/pages/CodeEditor.js
@@ -1,8 +1,38 @@
-import React from 'react'
+import React , {useState} from 'react'
+import Client from '../components/Client';
+import Editor from '../components/Editor';
 
 const CodeEditor = () => {
+  const [clients, setclients] = useState([{socketId:1 , username :"Harshal Mehta"},
+                                          {socketId:2 , username :"Meet Shukla"}
+                                        ]);
+
   return (
-    <div>Code Editor</div>
+  <div className='mainWrap'>
+    <div className='aside'>
+      <div className='asideInner'>
+        <div className='logo'>
+            <img className='logoImage'
+             src='/code-sync.png' 
+             alt = "logo"
+            />
+        </div>
+        <h3>Connected</h3>
+        <div className='clientsList'>
+        {clients.map((client)=>
+          (
+           <Client key= {client.socketId} username ={client.username} /> 
+          ))
+        }
+       </div>
+      </div>
+      <button className='btn copyBtn'>Copy Room ID</button>
+      <button className='btn leaveBtn'>Leave</button>
+    </div>
+    <div className='editorWrap'>
+        <Editor />
+    </div>
+  </div>
   )
 }
 export default CodeEditor
