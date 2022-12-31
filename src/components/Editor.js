a83a9b64b10dba8753c002d49e0e9871e3d09f7a
hrshlmeht <harshalmehta1998@gmail.com>
2022-12-30 23:37:20 -0500

    stlying hot nahie

--- src/components/Editor.js
+++ src/components/Editor.js
@@ -1,9 +1,31 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import Codemirror from 'codemirror';
+import 'codemirror/lib/codemirror.css'
+import 'codemirror/mode/javascript/javascript.js'
+import 'codemirror/theme/dracula.css'
+import 'codemirror/addon/edit/closetag'
+import 'codemirror/addon/edit/closebrackets'
+
 
 const Editor = () => {
-  return (
-    <div>Editor goes here </div>
-  )
+
+  useEffect(() => {
+
+    async function init(){
+        Codemirror.fromTextArea(document.getElementById('realtimeEditor'), {
+          mode: {name:'javascript', json:true},
+          theme : 'dracula',
+          autoCloseTags : true,
+          autoCloseBrackets : true,
+          lineNumbers : true,
+
+        })
+    }
+
+  }
+  ,[])
+
+  return <textarea id = 'realtimeEditor'></textarea>
 }
 
 export default Editor
